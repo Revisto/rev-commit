d7e9c5821634cf5057e73a85d6ca140a9fc524db
Revisto <theRevisto@gmail.com>
2024-09-29 18:03:10 +0330

    🎨 Improve Input Handling: Capture Multi-Line Changes
    - 🛠️ Added support for multi-line input descriptions.
    - ✨ Display prompt instructions using console.print.
    - 🔄 Imported readline for enhanced input functionality.

--- rev_commit/rev_commit.py
+++ rev_commit/rev_commit.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 
-from rich.prompt import Prompt
+import readline  # noqa
 from rich.console import Console
 from rev_commit.commit_generator import generate_commit_message
 
 console = Console()
 
-
 def main():
     try:
-        changes_description = Prompt.ask("Enter the description of changes")
+        console.print("[bold green]Enter the changes description (end with two consecutive empty lines):[/bold green]")
+        lines = []
+        empty_line_count = 0
+        while True:
+            line = input()
+            if line == "":
+                empty_line_count += 1
+                if empty_line_count == 2:
+                    break
+            else:
+                empty_line_count = 0
+                lines.append(line)
+        console.print(f"[bold green]Generating commit message...[/bold green]")
+        changes_description = "\n".join(lines)
+        
         commit_message = generate_commit_message(changes_description)
         console.print(
             f"[bold green]Generated Commit Message:[/bold green] "
@@ -17,7 +30,6 @@ def main():
         )
     except Exception as e:
         console.print(f"[bold red]Error:[/bold red] {str(e)}")
-
 
 if __name__ == "__main__":
     main()
